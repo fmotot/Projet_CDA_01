27ca504d52eeef156f094332f0096c15aa2a4dcb
Macorigh <ralpina@hotmail.com>
2020-05-11 16:26:07 +0200

    Creation classe Retrait

---
+++ src/fr/eni/trocencheres/bo/Retrait.java
@@ -0,0 +1,59 @@
+package fr.eni.trocencheres.bo;
+
+import java.io.Serializable;
+
+public class Retrait implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	String rue, ville;
+	Integer codePostal;
+	public Retrait(String rue, String ville, Integer codePostal) {
+		this.rue = rue;
+		this.ville = ville;
+		this.codePostal = codePostal;
+	}
+	public Retrait() {
+		}
+	/**
+	 * @return the rue
+	 */
+	public String getRue() {
+		return rue;
+	}
+	/**
+	 * @param rue the rue to set
+	 */
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	/**
+	 * @return the ville
+	 */
+	public String getVille() {
+		return ville;
+	}
+	/**
+	 * @param ville the ville to set
+	 */
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	/**
+	 * @return the codePostal
+	 */
+	public Integer getCodePostal() {
+		return codePostal;
+	}
+	/**
+	 * @param codePostal the codePostal to set
+	 */
+	public void setCodePostal(Integer codePostal) {
+		this.codePostal = codePostal;
+	}
+	@Override
+	public String toString() {
+		return "Retrait [rue=" + rue + ", ville=" + ville + ", codePostal=" + codePostal + "]";
+	}
+	
+	
+}
