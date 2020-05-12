7f1bf5eb5314fe52e37ed857df869c820ef7422a
Macorigh <ralpina@hotmail.com>
2020-05-12 13:34:31 +0200

    MAJ UtilisateurManager

--- src/fr/eni/trocencheres/bll/UtilisateurManager.java
+++ src/fr/eni/trocencheres/bll/UtilisateurManager.java
@@ -4,28 +4,6 @@ import fr.eni.trocencheres.bo.Utilisateur;
 
 public interface UtilisateurManager {
 
-	/**
-	 * @param utilisateur
-	 *  ajout d'un utilisateur a la BDD
-	 */
-	public void addUtilisateur(Utilisateur utilisateur);
 	
-	/**
-	 * @param noUtilisateur
-	 *  Suppression d'un utilisateur via son id
-	 */
-	public void deleteUtilisateur(Integer noUtilisateur);
-	
-	/**
-	 * @param pseudo
-	 * Selection d'un utilisateur via son pseudo
-	 */
-	public void selectUtilisateur(String pseudo);
-	
-	/**
-	 * @param utilisateur
-	 * Mise a jour des information de l'utilisateur
-	 */
-	public void updateUtilisateur(Utilisateur utilisateur);
-	
+	Utilisateur  login(String login, String motDePasse);
 }
