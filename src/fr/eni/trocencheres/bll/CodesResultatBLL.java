575e355fc163f6d62453db587f2bca83a8239c65
Frédérick <fmotot@gmail.com>
2020-05-14 14:48:24 +0200

    Mise à jour des messages d'erreurs

--- src/fr/eni/trocencheres/bll/CodesResultatBLL.java
+++ src/fr/eni/trocencheres/bll/CodesResultatBLL.java
@@ -15,10 +15,6 @@ public abstract class CodesResultatBLL {
 	 */
 	public static final int LOGIN_INCORRECT = 20002;
 	
-	/**
-	 * Echec lors de la sélection du type d'encodage
-	 */
-	public static final int ERREUR_SELECTION_ENCODAGE = 22002;
 	
 	/**
 	 * Echec téléphone déjà utilisé
@@ -36,7 +32,7 @@ public abstract class CodesResultatBLL {
 	public static final int REGLE_UTILISATEUR_CODE_POSTAL_VIDE = 21003;
 
 	/**
-	 * Echec code postal trop long
+	 * Echec code postal trop long +10
 	 */
 	public static final int REGLE_UTILISATEUR_CODE_POSTAL_TROP_LONG = 21004;
 
@@ -59,7 +55,6 @@ public abstract class CodesResultatBLL {
 	 * Echec nom trop long +30
 	 */
 	public static final int REGLE_UTILISATEUR_NOM_TROP_LONG = 21008;
-	
 	
 	/**
 	 * Echec prenom obligatoire
@@ -105,4 +100,10 @@ public abstract class CodesResultatBLL {
 	 * Echec ville trop long +30
 	 */
 	public static final int REGLE_UTILISATEUR_VILLE_TROP_LONG = 21017;
+
+	
+	/**
+	 * Echec lors de la sélection du type d'encodage
+	 */
+	public static final int ERREUR_SELECTION_ENCODAGE = 22001;
 }
