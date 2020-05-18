4b05974ae65e28daba8031a66bc3becda2f0eb1b
Frédérick <fmotot@gmail.com>
2020-05-18 14:05:53 +0200

    Ajout de l'auteur fmotot

--- src/fr/eni/trocencheres/bll/CategorieManager.java
+++ src/fr/eni/trocencheres/bll/CategorieManager.java
@@ -5,6 +5,11 @@ import java.util.List;
 import fr.eni.trocencheres.BusinessException;
 import fr.eni.trocencheres.bo.Categorie;
 
+/**
+ * 
+ * @author fmoto
+ *
+ */
 public interface CategorieManager {
 
 	List<Categorie> getListeCategorie() throws BusinessException;
