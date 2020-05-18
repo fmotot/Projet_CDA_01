0c942e0016257af1ad79566c9c1d18e300e8d35a
Frédérick <fmotot@gmail.com>
2020-05-18 10:45:44 +0200

    Modification de la gestion de la bidirectionnalité vente/encheres

--- src/fr/eni/trocencheres/bo/Enchere.java
+++ src/fr/eni/trocencheres/bo/Enchere.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+/**
+ * 
+ * @author fmoto
+ *
+ */
 public class Enchere implements Serializable{
 	/**
 	 * 
@@ -29,7 +34,7 @@ public class Enchere implements Serializable{
 	public Enchere(Utilisateur acheteur, Vente vente, int mise) {
 		this.dateEnchere = LocalDateTime.now(ZoneId.of("Europe/Paris"));;
 		this.acheteur = acheteur;
-		this.vente = vente;
+		this.setVente(vente);
 		this.mise = mise;
 	}
 	
@@ -42,7 +47,7 @@ public class Enchere implements Serializable{
 	public Enchere(LocalDateTime dateEnchere, Utilisateur acheteur, Vente vente, int mise) {
 		this.dateEnchere = dateEnchere;
 		this.acheteur = acheteur;
-		this.vente = vente;
+		this.setVente(vente);
 		this.mise = mise;
 	}
 	
@@ -56,46 +61,69 @@ public class Enchere implements Serializable{
 	public LocalDateTime getDateEnchere() {
 		return dateEnchere;
 	}
+	
 	/**
 	 * @param dateEnchere the dateEnchere to set
 	 */
 	public void setDateEnchere(LocalDateTime dateEnchere) {
 		this.dateEnchere = dateEnchere;
 	}
+	
 	/**
 	 * @return the acheteur
 	 */
 	public Utilisateur getAcheteur() {
 		return acheteur;
 	}
+	
 	/**
 	 * @param acheteur the acheteur to set
 	 */
 	public void setAcheteur(Utilisateur acheteur) {
 		this.acheteur = acheteur;
 	}
+	
 	/**
 	 * @return the vente
 	 */
 	public Vente getVente() {
 		return vente;
 	}
+	
 	/**
 	 * @param vente the vente to set
 	 */
 	public void setVente(Vente vente) {
 		this.vente = vente;
+		vente.addEnchere(this);
 	}
+	
 	/**
 	 * @return the mise
 	 */
 	public int getMise() {
 		return mise;
 	}
+	
 	/**
 	 * @param mise the mise to set
 	 */
 	public void setMise(int mise) {
 		this.mise = mise;
 	}
+	
+	@Override
+	public boolean equals(Object object) {
+		boolean equal = false;
+		
+		if (object instanceof Enchere 
+				&& this.getAcheteur().getNoUtilisateur() == ((Enchere)object).getAcheteur().getNoUtilisateur()
+				&& this.getVente().getNoVente() == ((Enchere)object).getVente().getNoVente()) {
+			equal = true;
+		}
+
+		
+		return equal;
+	}
+	
 }
