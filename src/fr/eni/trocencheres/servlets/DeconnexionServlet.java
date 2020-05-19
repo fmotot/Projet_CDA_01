16d80b1cf95d5147216add7df1da4cb4dc468469
Jean <jean.rupin@gmail.com>
2020-05-19 17:23:50 +0200

    Amelioration Header + servlet Deconnexion

---
+++ src/fr/eni/trocencheres/servlets/DeconnexionServlet.java
@@ -0,0 +1,40 @@
+package fr.eni.trocencheres.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class DeconnexionServlet
+ */
+@WebServlet("/Deconnexion")
+public class DeconnexionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.setAttribute("utilisateur", null);
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("WEB-INF/jsp/ListeEnchere.jsp");
+		requestDispatcher.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
